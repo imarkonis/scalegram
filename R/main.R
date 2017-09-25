170816657a5a4019f567517076dcae65779c629e
Yannis Markonis <imarkonis@gmail.com>
2017-09-25 16:43:01 +0200

    Trying to work to 'develop' branch both locally and remotely

--- R/main.R
+++ R/main.R
@@ -88,7 +88,7 @@ plot_scalegram = function(X){
 # White noise (WN)
 generate_wn = function(sigma, delta){
   stdev = sigma/(sqrt(delta))
-  return(stdev^2)
+  return(stdev ^ 2)
 }
 # AR(1) process
 generate_ar_1 = function(sigma, delta, rho){
@@ -104,7 +104,7 @@ generate_fgn = function(sigma, delta, rho){
 # Harmonics
 generate_harmonic = function(delta, period){
   stdev = (period/(pi*delta))*abs(sin((pi*delta)/period))
-  return(stdev^2)
+  return(stdev ^ 2)
 }
 
 
