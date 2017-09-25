4f70afe23bbeedd9cfbe690ea3d56fcd50f8f96a
Yannis Markonis <imarkonis@gmail.com>
2017-09-25 16:29:16 +0200

    Typos all over!

--- R/main.R
+++ R/main.R
@@ -93,13 +93,13 @@ generate_wn = function(sigma, delta){
 # AR(1) process
 generate_ar_1 = function(sigma, delta, rho){
   stdev = (sigma/(sqrt(delta))) * sqrt(((1-rho^2)-(2*rho*(1-rho^delta))/delta)/((1-rho)^2))
-  return(stdev^2)
+  return(stdev ^ 2)
 }
 # FGN process
 generate_fgn = function(sigma, delta, rho){
-  H=0.5*(log2(rho+1)+1)
-  stdev = (delta^(H-1)) * sigma
-  return(stdev^2)
+  H = 0.5 * (log2(rho + 1) + 1)
+  stdev = (delta ^ (H - 1)) * sigma
+  return(stdev ^ 2)
 }
 # Harmonics
 generate_harmonic = function(delta, period){
