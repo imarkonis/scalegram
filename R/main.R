c4c6b419c32a93d754449182d584b388ea889bec
Yannis Markonis <imarkonis@gmail.com>
2017-10-10 18:57:35 +0200

    Removed 2nd plot

--- R/main.R
+++ R/main.R
@@ -1,6 +1,6 @@
 ## Empirical scalegrams
 
-scalegram_main <- function(x, MODE="s1", STD=TRUE, threshold=30){ #Right
+scalegram_main <- function(x, MODE = "s1", STD = TRUE, threshold = 30){ #Right
   library(data.table) # load libraries inside for the 'parallel' package
   library(moments)
   library(Lmoments)
@@ -100,7 +100,7 @@ scalegram_main <- function(x, MODE="s1", STD=TRUE, threshold=30){ #Right
 
 ## Plot scalegram ==============================================================
 ##==============================================================================
-plot_scalegram = function(X, MODE){
+plot_scalegram = function(X, ...){
 
   '%!in%' <- function(x,y)!('%in%'(x,y)) # keep function inside for the 'parallel' package
 
@@ -163,49 +163,6 @@ rescale_variance = function(emp_scalegram_coarse, emp_scalegram_fine, scale_rati
   dummy$scale = dummy$scale * scale_ratio
   dummy$y_scale = t(t(dummy$y_scale) * rescale_factor)
   return(dummy)
-}
-
-## Plot scalegram
-plot_scalegram = function(X){
-  if(is.ts(X)) {X = scalegram_main(as.vector(X))
-  } else if(is.vector(X)) {X = scalegram_main(X)
-  }
-  if("variable" %in% colnames(X)){
-
-    if(length(unique(X$variable))>10){
-    aa = 0.2
-    } else {
-    aa=1
-    }
-    ggplot(data=X, aes(x=scale, y=y_scale))+
-      geom_line(aes(group=interaction(variable),
-                    colour = variable), show.legend = FALSE, size = 0.5, alpha = aa) +
-      geom_point(aes(group=interaction(variable),
-                     colour=variable), show.legend = FALSE, alpha = aa) +
-      geom_tile(aes(fill=variable))+
-      scale_y_log10("Variance [-]",
-                    labels = trans_format("log10", math_format(10^.x))) +
-      scale_x_log10("Aggregation scale [-]",
-                    labels = trans_format("log10", math_format(10^.x))) +
-      annotation_logticks(sides = "bl") +
-      theme_bw()+
-      theme(panel.grid.minor.x = element_blank(),
-            panel.grid.minor.y = element_blank())+
-      coord_fixed()
-  } else {
-    ggplot(data=X, aes(x=scale, y=y_scale))+
-      geom_line(show.legend = FALSE, size = 0.5) +
-      geom_point(show.legend = FALSE) +
-      scale_y_log10("Variance [-]",
-                    labels = trans_format("log10", math_format(10^.x))) +
-      scale_x_log10("Aggregation scale [-]",
-                    labels = trans_format("log10", math_format(10^.x))) +
-      annotation_logticks(sides = "bl") +
-      theme_bw()+
-      theme(panel.grid.minor.x = element_blank(),
-            panel.grid.minor.y = element_blank())+
-      coord_fixed()
-  }
 }
 
 ## Theoretical scalegrams
