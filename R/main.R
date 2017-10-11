89d1e9bb22dfcf0cb2080469a85cd78a66dbaa5c
Yannis Markonis <imarkonis@gmail.com>
2017-10-11 11:04:34 +0200

    Removed data.table and changed var names

--- R/main.R
+++ R/main.R
@@ -1,91 +1,99 @@
 ## Empirical scalegrams
 
-scalegram <- function(x, MODE = "s1", STD = TRUE, threshold = 30){ #Right
+scalegram <- function(x, MODE = "s1", STD = TRUE, threshold = 30){ #STD: standardize the scalegram, threshold: sample size at last scale
   library(data.table) # load libraries inside for the 'parallel' package
   library(moments)
   library(Lmoments)
   '%!in%' <- function(x,y)!('%in%'(x,y)) # keep function inside for the 'parallel' package
   # check if correct MODE is provided
-  if(MODE %!in% c("mu", "s1", "s2", "s3", "s4", "CV",
-                  "L1", "L2", "t2", "t3", "t4")){
+  if (MODE %!in% c("mu", "s1", "s2", "s3", "s4", "CV",
+                   "L1", "L2", "t2", "t3", "t4")){
     return("Error: Invalid MODE. Select one of s1, s2, s3, s4, CV, L1, L2, t2, t3, t4")
+
   } else {
     nna <- sum(!is.na(x)) # actual length without accounting for missing values
-    delta <- round(nna/threshold, 0) # aggregrion scale up to 30% of the sample size does not count NAs
+    max_agg_scale <- round(nna/threshold, 0) # aggregrion scale up to 30% of the sample size does not count NAs
 
-    if (delta!=0 & nna > 2 * threshold){ # check for adequate time series length
-      scaledf <- data.frame(scale=1:delta, y_scale=NA) # Create a data.frame to store all the y_scale
+    if (max_agg_scale != 0 & nna > 2 * threshold){ # check for adequate time series length
+      scale_df <- data.frame(scale = 1:max_agg_scale, y_scale = NA) # Create a data.frame to store all the y_scale
 
       if (STD == TRUE){ # standardize data ,i.e., zero mean unit variance at the original time scale
-        samp <- data.table(scale(x, center = TRUE, scale = TRUE))
-      } else {
-        samp <- data.table(x)
+        samp <- scale(x, center = TRUE, scale = TRUE)
       }
 
       # normal moments ------------------------------------------------------------
       if (MODE == "mu"){ # MODE: mean
-        scaledf[1,"y_scale"]    <- mean(samp$V1, na.rm=T)
+        scale_df[1,"y_scale"] <- mean(x, na.rm=T)
+
       } else if (MODE == "s1"){ # MODE: standard deviation
-        scaledf[1,"y_scale"]    <- sd(samp$V1, na.rm=T)
+        scale_df[1,"y_scale"] <- sd(x, na.rm=T)
+
       } else if (MODE == "s2"){ # MODE: variance
-        scaledf[1,"y_scale"]    <- var(samp$V1, na.rm=T)
+        scale_df[1,"y_scale"] <- var(x, na.rm=T)
+
       } else if (MODE == "s3"){ # MODE: skewness
-        scaledf[1,"y_scale"]    <- skewness(samp$V1, na.rm=T)
+        scale_df[1,"y_scale"] <- skewness(x, na.rm=T)
+
       } else if (MODE == "s4"){ # MODE: kurtosis
-        scaledf[1,"y_scale"]    <- kurtosis(samp$V1, na.rm=T)
+        scale_df[1,"y_scale"] <- kurtosis(x, na.rm=T)
+
       } else if (MODE == "CV"){ # MODE: coefficient of variation
-        scaledf[1,"y_scale"]    <- sd(samp$V1, na.rm=T)/mean(samp$V1, na.rm=T)
+        scale_df[1,"y_scale"] <- sd(x, na.rm=T)/mean(x, na.rm=T)
+
         # L-moments -----------------------------------------------------------------
       } else if (MODE == "L1"){ # MODE: L1, L-mean, L-location
-        scaledf[1,"y_scale"]    <- Lmoments(samp$V1, rmax=2,na.rm=T)[,"L1"]
+        scale_df[1,"y_scale"] <- Lmoments(x, rmax=2, na.rm=T)[, "L1"]
+
       } else if (MODE == "L2"){ # MODE: L2, L-scale
-        scaledf[1,"y_scale"]    <- Lmoments(samp$V1, rmax=2,na.rm=T)[,"L2"]
+        scale_df[1,"y_scale"] <- Lmoments(x, rmax=2, na.rm=T)[, "L2"]
+
       } else if (MODE == "t2"){ # MODE: L-moment ratio L2/L1
-        scaledf[1,"y_scale"]    <- Lmoments(samp$V1, rmax=2,na.rm=T)[,"L2"]/Lmoments(samp$V1, rmax=2,na.rm=T)[,"L1"]
+        scale_df[1,"y_scale"] <- Lmoments(x, rmax=2, na.rm=T)[, "L2"] / Lmoments(x, rmax=2, na.rm=T)[, "L1"]
+
       } else if (MODE == "t3"){ # MODE: L-moment ratio L3/L2
-        scaledf[1,"y_scale"]    <- Lcoefs(samp$V1,rmax=4,na.rm=T)[,"tau3"]
+        scale_df[1,"y_scale"] <- Lcoefs(x, rmax=4, na.rm=T)[, "tau3"]
+
       } else if (MODE == "t4"){ # MODE: L-moment ratio L4/L3
-        scaledf[1,"y_scale"]    <- Lcoefs(samp$V1,rmax=4,na.rm=T)[,"tau4"]
+        scale_df[1, "y_scale"] <- Lcoefs(x, rmax=4, na.rm=T)[, "tau4"]
       }
 
-      for (i in 2:delta){ # loop around aggregation scale
-        dummy <- rep(1:floor(nna/i), each=i)
-        ttest <- samp[1:length(dummy), list(AVERAGE=mean(V1)), by=list(dummy)]$AVERAGE
-        if (sum(!is.na(ttest))>=threshold){ # have at least 30 values for the y_scale estimation     # normal moments -------------------------------------------------------
+      for (i in 2:max_agg_scale){ # loop around aggregation scale
+        x_agg = as.numeric(tapply(x, (seq_along(x) - 1) %/% i, mean, na.rm = T))
+
+        if (sum(!is.na(x_agg))>=threshold){ # have at least 30 values for the y_scale estimation     # normal moments -------------------------------------------------------
           if (MODE == "mu"){ # MODE: mean
-            dummyscale <- mean(ttest, na.rm=T)
-            scaledf[i,"y_scale"] <- dummyscale
+            scale_df[i,"y_scale"] <- mean(x_agg, na.rm=T)
+
           } else if (MODE == "s1"){ # MODE: standard deviation
-            dummyscale <- sd(ttest, na.rm=T)
-            scaledf[i,"y_scale"] <- dummyscale
+            scale_df[i,"y_scale"] <- sd(x_agg, na.rm=T)
+
           } else if (MODE == "s2"){ # MODE: variance
-            dummyscale <- var(ttest, na.rm=T)
-            scaledf[i,"y_scale"] <- dummyscale
+            scale_df[i,"y_scale"] <- var(x_agg, na.rm=T)
+
           } else if (MODE == "s3"){ # MODE: skewness
-            dummyscale <- skewness(ttest, na.rm=T)
-            scaledf[i,"y_scale"] <- dummyscale
+            scale_df[i,"y_scale"] <- skewness(x_agg, na.rm=T)
+
           } else if (MODE == "s4"){ # MODE: kurtosis
-            dummyscale <- kurtosis(ttest, na.rm=T)
-            scaledf[i,"y_scale"] <- dummyscale
+            scale_df[i,"y_scale"] <- kurtosis(x_agg, na.rm=T)
+
           } else if (MODE == "CV"){ # MODE: coefficient of variation
-            dummyscale <- sd(ttest, na.rm=T)/mean(ttest, na.rm=T)
-            scaledf[i,"y_scale"] <- dummyscale
+            scale_df[i,"y_scale"] <- sd(x_agg, na.rm=T)/mean(x_agg, na.rm=T)
+
             # L-moments ------------------------------------------------------------
           } else if (MODE == "L1"){ # MODE: L1, L-mean, L-location
-            dummyscale <- Lmoments(ttest, rmax=2,na.rm=T)[,"L1"]
-            scaledf[i,"y_scale"] <- dummyscale
+            scale_df[i,"y_scale"] <- Lmoments(x_agg, rmax=2,na.rm=T)[,"L1"]
+
           } else if (MODE == "L2"){ # MODE: L2, L-scale
-            dummyscale <- Lmoments(ttest, rmax=2,na.rm=T)[,"L2"]
-            scaledf[i,"y_scale"] <- dummyscale
+            scale_df[i,"y_scale"] <- Lmoments(x_agg, rmax=2,na.rm=T)[,"L2"]
+
           } else if (MODE == "t2"){ # MODE: L-moment ratio L2/L1
-            dummyscale <- Lmoments(ttest, rmax=2,na.rm=T)[,"L2"]/Lmoments(ttest, rmax=2,na.rm=T)[,"L1"]
-            scaledf[i,"y_scale"] <- dummyscale
+            scale_df[i,"y_scale"] <-  Lmoments(x_agg, rmax=2,na.rm=T)[,"L2"]/Lmoments(x_agg, rmax=2,na.rm=T)[,"L1"]
+
           } else if (MODE == "t3"){ # MODE: L-moment ratio L3/L2
-            dummyscale <- Lcoefs(ttest,rmax=4,na.rm=T)[,"tau3"]
-            scaledf[i,"y_scale"] <- dummyscale
+            scale_df[i,"y_scale"] <- Lcoefs(x_agg,rmax=4,na.rm=T)[,"tau3"]
+
           } else if (MODE == "t4"){ # MODE: L-moment ratio L4/L3
-            dummyscale <- Lcoefs(ttest,rmax=4,na.rm=T)[,"tau4"]
-            scaledf[i,"y_scale"] <- dummyscale
+            scale_df[i,"y_scale"] <- Lcoefs(x_agg,rmax=4,na.rm=T)[,"tau4"]
           }
         }
       } # loop around aggregation scale
@@ -93,23 +101,17 @@ scalegram <- function(x, MODE = "s1", STD = TRUE, threshold = 30){ #Right
     }else{ # i.e., too short time series, return NAs
       return("Error: Time series length too short!")
     }
-    return(scaledf[complete.cases(scaledf),])
+    return(scale_df[complete.cases(scale_df),])
   }
 }
 
 scalegram_parallel = function(x, MODE, STD=TRUE, threshold=30, cores_used = detectCores() - 1){
   cl = makeCluster(cores_used)
-  clusterExport(cl=cl, varlist=c("x", "MODE", "STD", "threshold", "scalegram"), envir=environment())
-  out  = parApply(cl, as.matrix(x), 2, MODE=MODE, STD=TRUE, threshold=30, scalegram)
+  clusterExport(cl = cl, varlist=c("x", "MODE", "STD", "threshold", "scalegram"), envir = environment())
+  out  = parApply(cl, as.matrix(x), 2, MODE=MODE, STD=TRUE, threshold = 30, scalegram)
   return(out)
 }
 
-scalegram_parallel_old = function(x, cores_used = detectCores() - 1){
-  cl = makeCluster(cores_used)
-  clusterExport(cl=cl, varlist=c("x", "scalegram"), envir=environment())
-  out  = parApply(cl, as.matrix(x), 2, scalegram)
-  return(out)
-}
 
 
 
