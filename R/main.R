c3f114d1607d0bd7dd185471047d0cad9414f4a1
Yannis Markonis <imarkonis@gmail.com>
2017-10-25 17:55:10 +0200

    Building documentation

--- R/main.R
+++ R/main.R
@@ -1,21 +1,21 @@
-#' Main scalegram function
+#' Main scalegram function called by scalegram.
 #'
 #' @param x A vector, time series or a matrix.
-#' @param MODE the statistic used.
-#' @return A list with the scalegram of \code{x} for statistic \code{MODE} and the corresponding plot [ggplot object].
+#' @param stat the statistic used.
+#' @return A list with the scalegram of \code{x} for statistic \code{stat} and the corresponding plot [ggplot object].
 #' @examples
 #' scalegram(dataset, "s1")
 #' scalegram(dataset[,1], "s2")
 #' @export
 
-scalegram_main <- function(x, MODE, STD, threshold){ #STD: standardize the scalegram, threshold: sample size at last scale
+scalegram_main <- function(x, stat, std, threshold){ #std: standardize the scalegram, threshold: sample size at last scale
   library(moments) # load libraries inside for the 'parallel' package
   library(Lmoments)
   '%!in%' <- function(x,y)!('%in%'(x,y)) # keep function inside for the 'parallel' package
-  # check if correct MODE is provided
-  if (MODE %!in% c("mu", "s1", "s2", "s3", "s4", "CV",
+  # check if correct stat is provided
+  if (stat %!in% c("mu", "s1", "s2", "s3", "s4", "CV",
                    "L2", "t2", "t3", "t4"))
-    stop("Error: Invalid MODE. Select one of mu, s1, s2, s3, s4, CV, L2, t2, t3, t4.")
+    stop("Error: Invalid stat. Select one of mu, s1, s2, s3, s4, CV, L2, t2, t3, t4.")
 
   nna <- sum(!is.na(x)) # actual length without accounting for missing values
   max_agg_scale <- round(nna/threshold, 0) # aggregrion scale up to 30% of the sample size does not count NAs
@@ -23,31 +23,31 @@ scalegram_main <- function(x, MODE, STD, threshold){ #STD: standardize the scale
   if (max_agg_scale != 0 & nna > 2 * threshold){ # check for adequate time series length
     scale_df <- data.frame(scale = 1:max_agg_scale, y_scale = NA) # Create a data.frame to store all the y_scale
 
-    if (STD == TRUE){ # standardize data ,i.e., zero mean unit variance at the original time scale
+    if (std == TRUE){ # standardize data ,i.e., zero mean unit variance at the original time scale
       x <- scale(x, center = TRUE, scale = TRUE)
     }
 
     # classic moments ------------------------------------------------------------
-    if (MODE == "mu"){ # MODE: mean
+    if (stat == "mu"){ # stat: mean
       scale_df[1,"y_scale"] <- mean(x, na.rm=T)
-    } else if (MODE == "s1"){ # MODE: standard deviation
+    } else if (stat == "s1"){ # stat: standard deviation
       scale_df[1,"y_scale"] <- sd(x, na.rm=T)
-    } else if (MODE == "s2"){ # MODE: variance
+    } else if (stat == "s2"){ # stat: variance
       scale_df[1,"y_scale"] <- var(x, na.rm=T)
-    } else if (MODE == "s3"){ # MODE: skewness
+    } else if (stat == "s3"){ # stat: skewness
       scale_df[1,"y_scale"] <- skewness(x, na.rm=T)
-    } else if (MODE == "s4"){ # MODE: kurtosis
+    } else if (stat == "s4"){ # stat: kurtosis
       scale_df[1,"y_scale"] <- kurtosis(x, na.rm=T)
-    } else if (MODE == "CV"){ # MODE: coefficient of variation
+    } else if (stat == "CV"){ # stat: coefficient of variation
       scale_df[1,"y_scale"] <- sd(x, na.rm=T)/mean(x, na.rm=T)
     # L-moments -----------------------------------------------------------------
-    } else if (MODE == "L2"){ # MODE: L2, L-scale
+    } else if (stat == "L2"){ # stat: L2, L-scale
       scale_df[1,"y_scale"] <- Lmoments(x, rmax=2, na.rm=T)[, "L2"]
-    } else if (MODE == "t2"){ # MODE: L-moment ratio L2/L1
+    } else if (stat == "t2"){ # stat: L-moment ratio L2/L1
       scale_df[1,"y_scale"] <- Lmoments(x, rmax=2, na.rm=T)[, "L2"] / Lmoments(x, rmax=2, na.rm=T)[, "L1"]
-    } else if (MODE == "t3"){ # MODE: L-moment ratio L3/L2
+    } else if (stat == "t3"){ # stat: L-moment ratio L3/L2
       scale_df[1,"y_scale"] <- Lcoefs(x, rmax=4, na.rm=T)[, "tau3"]
-    } else if (MODE == "t4"){ # MODE: L-moment ratio L4/L3
+    } else if (stat == "t4"){ # stat: L-moment ratio L4/L3
       scale_df[1, "y_scale"] <- Lcoefs(x, rmax=4, na.rm=T)[, "tau4"]
     }
 
@@ -56,35 +56,35 @@ scalegram_main <- function(x, MODE, STD, threshold){ #STD: standardize the scale
 
       if (sum(!is.na(x_agg))>=threshold){ # have at least 30 values for the y_scale estimation
           # classic moments -------------------------------------------------------
-          if (MODE == "mu"){ # MODE: mean
+          if (stat == "mu"){ # stat: mean
             scale_df[i,"y_scale"] <- mean(x_agg, na.rm=T)
 
-          } else if (MODE == "s1"){ # MODE: standard deviation
+          } else if (stat == "s1"){ # stat: standard deviation
             scale_df[i,"y_scale"] <- sd(x_agg, na.rm=T)
 
-          } else if (MODE == "s2"){ # MODE: variance
+          } else if (stat == "s2"){ # stat: variance
             scale_df[i,"y_scale"] <- var(x_agg, na.rm=T)
 
-          } else if (MODE == "s3"){ # MODE: skewness
+          } else if (stat == "s3"){ # stat: skewness
             scale_df[i,"y_scale"] <- skewness(x_agg, na.rm=T)
 
-          } else if (MODE == "s4"){ # MODE: kurtosis
+          } else if (stat == "s4"){ # stat: kurtosis
             scale_df[i,"y_scale"] <- kurtosis(x_agg, na.rm=T)
 
-          } else if (MODE == "CV"){ # MODE: coefficient of variation
+          } else if (stat == "CV"){ # stat: coefficient of variation
             scale_df[i,"y_scale"] <- sd(x_agg, na.rm=T)/mean(x_agg, na.rm=T)
 
             # L-moments ------------------------------------------------------------
-          } else if (MODE == "L2"){ # MODE: L2, L-scale
+          } else if (stat == "L2"){ # stat: L2, L-scale
             scale_df[i,"y_scale"] <- Lmoments(x_agg, rmax=2,na.rm=T)[,"L2"]
 
-          } else if (MODE == "t2"){ # MODE: L-moment ratio L2/L1
+          } else if (stat == "t2"){ # stat: L-moment ratio L2/L1
             scale_df[i,"y_scale"] <-  Lmoments(x_agg, rmax=2,na.rm=T)[,"L2"]/Lmoments(x_agg, rmax=2,na.rm=T)[,"L1"]
 
-          } else if (MODE == "t3"){ # MODE: L-moment ratio L3/L2
+          } else if (stat == "t3"){ # stat: L-moment ratio L3/L2
             scale_df[i,"y_scale"] <- Lcoefs(x_agg,rmax=4,na.rm=T)[,"tau3"]
 
-          } else if (MODE == "t4"){ # MODE: L-moment ratio L4/L3
+          } else if (stat == "t4"){ # stat: L-moment ratio L4/L3
             scale_df[i,"y_scale"] <- Lcoefs(x_agg,rmax=4,na.rm=T)[,"tau4"]
           }
         }
--- R/scalegram.R
+++ R/scalegram.R
@@ -1,27 +1,28 @@
-#' Main scalegram function
-#'
+#' @title Determine and plot scalegram
+#' @description Estimates scalegram(s) of specific statistics and plots the result.
 #' @param x A vector, time series or a matrix.
-#' @param MODE the statistic used.
-#' @return A list with the scalegram of \code{x} for statistic \code{MODE} and the corresponding plot [ggplot object].
+#' @param stat The statistic used (see Details).
+#' @param std If TRUE (default) standardize the scalegram to unit.
+#' @param threshold Sample size at last scale (see Details).
+#' @param plot If TRUE (default) the scalegram is also plotted.
+#' @return A list with the scalegram of \code{x} for statistic \code{stat} and the corresponding plot [ggplot object].
+#' @details Here are the details.
 #' @examples
 #' scalegram(dataset, "s1")
 #' scalegram(dataset[,1], "s2")
 #' @export
-
-scalegram = function(x, MODE = "s1", STD = TRUE, threshold = 30, PLOT = TRUE){
-
-  if (!is.numeric(x)) stop("x should be numeric.")
-  if (!is.vector(x) & !is.ts(x) & !is.matrix(x)) stop("x should be either vector, or time series, or matrix object.")
-
-  if(is.ts(x)) x = as.vector(x)
-
-  if(is.vector(x)) {
-    out = scalegram_main(x, MODE, STD, threshold)
+scalegram <-
+  function(x, stat = "s1", std = TRUE, threshold = 30, plot = TRUE){
+  if (!is.numeric(x)) stop ("x should be numeric.")
+  if (!is.vector(x) & !is.ts(x) & !is.matrix(x)) stop ("x should be either vector, or time series, or matrix object.")
+  if (is.ts(x)) x = as.vector(x)
+  if (is.vector(x)) {
+    out = scalegram_main(x, stat, std, threshold)
     out = out[complete.cases(out),]
     out$Variable = "variable"
-  } else {
-    out = scalegram_parallel(x, MODE, STD, threshold)
-
+  }
+  else {
+    out = scalegram_parallel(x, stat, std, threshold)
     # transform from 'list' to 'matrix'
     rows_diff <- max(sapply(out, function(x) max(x[[1]])))
     columns <- length(out)
@@ -37,22 +38,20 @@ scalegram = function(x, MODE = "s1", STD = TRUE, threshold = 30, PLOT = TRUE){
     colnames(out) = c("scale", "Variable", "y_scale")
     out = out[complete.cases(out),]
   }
-
-  if (PLOT == TRUE){
+  if (plot == TRUE){
 
     if(length(unique(out$Variable))>10){
       transp = 1/log(length(unique(out$Variable)))
     } else {
       transp = 1
     }
-
-    plot_sc = plot_scalegram(out, MODE, transparancy=transp)
+    plot_sc = plot_scalegram(out, stat, transparancy=transp)
     print(plot_sc)
-
     # return only complete cases
     return(list(scalegram_df = out,
                 scalegram_plot = plot_sc))
-  } else {
+  }
+  else {
     return(out)
   }
 }
