96ea60431d2ee7e3f5b6f3e7107ae0e1e21ea858
Yannis Markonis <imarkonis@gmail.com>
2017-10-30 15:48:31 +0100

    Working on documentation files

--- temp/examples.R
+++ temp/examples.R
@@ -2,3 +2,5 @@ test_dt = data.table(cbind(type = sample(c(0,1), size = 1000, replace = T), valu
 test_dt_scale = test_dt[, scalegram(value), type]
 
 plot_scalegram(test_dt_scale, INDEX = "type")
+
+library(githubinstall)
