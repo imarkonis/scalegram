9d7a2c4341649f055baa9b35177a1adf2c3b9761
Yannis Markonis <imarkonis@gmail.com>
2018-04-26 14:47:50 +0200

    * Added parallel computing in scalegram_main (for each)
    * Added the fast option [sd is estimated in logarithmic timescales]

--- R/scalegram.R
+++ R/scalegram.R
@@ -1,9 +1,7 @@
-scalegram  <- function(x, stat = "sd", std = T, threshold = 30, plot = T, ...) {
+scalegram  <- function(x, stat = "sd", std = T, threshold = 30, plot = T, fast = F, ...) {
     if (!is.numeric(x)) stop ("x should be numeric.")
     if (!is.vector(x)) stop ("x should be vector.")
-      out <- scalegram_main(x, stat, std, threshold)
-      out <- out[complete.cases(out),]
-      colnames(out)[2] = "value"
+      out <- scalegram_main(x, stat, std, threshold, fast)
     if (plot == T){
       plot_sc <- plot_scalegram(out, ...)
       return(list(sg_df   = out,
