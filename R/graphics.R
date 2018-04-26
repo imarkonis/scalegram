1462cce0b97759b11f4fc9ccc8f34577e3623277
Yannis Markonis <imarkonis@gmail.com>
2018-04-26 16:50:05 +0200

    typos

--- R/graphics.R
+++ R/graphics.R
@@ -14,10 +14,10 @@ plot_scalegram <- function(x, log_x = T, log_y = T){
                                                             function(x) 10 ^ x)) +
       annotation_logticks(sides = "b")
   } else {
-    gpp = gp + scale_x_continuous("Aggregation scale [-]")
+    gpp <- gp + scale_x_continuous("Aggregation scale [-]")
   }
   if(log_y == T){
-    gppp = gpp + scale_y_log10(colnames(df)[2], labels = trans_format("log10", math_format(10^.x)),
+    gppp <- gpp + scale_y_log10(colnames(df)[2], labels = trans_format("log10", math_format(10^.x)),
                                breaks = trans_breaks("log10",
                                                              n = abs(round(log10(min(df[, 2], na.rm = T)))) + 1,
                                                              function(x) 10 ^ x)) +
@@ -27,3 +27,4 @@ plot_scalegram <- function(x, log_x = T, log_y = T){
   }
   print(gppp)
 }
+
