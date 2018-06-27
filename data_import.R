ea198cf4ccea1289578d6e6fe8876cf0cc5b975c
Yannis Markonis <imarkonis@gmail.com>
2018-06-27 11:43:43 +0200

    include data

---
+++ data_import.R
@@ -0,0 +1,9 @@
+load('./data-raw/temporal.Rdata')
+devtools::use_data(cnrm_nl)
+devtools::use_data(gpm_nl)
+devtools::use_data(knmi_nl)
+devtools::use_data(rdr_nl)
+devtools::use_data(ncep_nl)
+
+load('./data-raw/spatial.Rdata')
+devtools::use_data(gpm_events)
